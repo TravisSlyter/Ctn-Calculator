2e255295f64732ee70add928fdf9b2eee8ae7ed0
Travis Slyter <travisslyter@gmail.com>
2021-01-28 15:10:12 -0700

    style js file

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+////////// Public Variables //////////////
+
+
+
+//////////// Objects & Arrays //////////////
+let boxes = {
+    b9x6x4: { length: '9', width: '6', height: '4', wgt: '3.9' }
+}
+
+
+let items = {
+    hl1038: { sku: '1038', desc: 'NAV Headlamp', wgt: '3.4' }
+}
+
+
+//////////// Public Functions ///////////////
+
+
+
+//////////// Start App ////////////////
+
+
+
+///////////// Testing /////////////////
